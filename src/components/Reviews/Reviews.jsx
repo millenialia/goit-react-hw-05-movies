499d79a14f5bf8f9016e7fa66ebf495b0d97c546
millenialia <lenalebo@gmail.com>
2023-08-02 22:06:03 +0200

    Update Reviews.jsx

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,4 +1,4 @@
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { fetchMovieReviewsById } from "../../services/api"
 
